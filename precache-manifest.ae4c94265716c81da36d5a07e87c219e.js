ab605e5e14d5e7bcd3ad308d3758e1ea5d81c298
Jorge Attie <attiejorge89@gmail.com>
2020-08-03 15:11:08 -0300

    Updates

---
+++ precache-manifest.ae4c94265716c81da36d5a07e87c219e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbadb47e472770474646e9f38b23d2d4",
+    "url": "/DevFlix/index.html"
+  },
+  {
+    "revision": "82685d8615557cd6424b",
+    "url": "/DevFlix/static/css/main.943f5163.chunk.css"
+  },
+  {
+    "revision": "738c699f343912e20b88",
+    "url": "/DevFlix/static/js/2.e461b27b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/DevFlix/static/js/2.e461b27b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82685d8615557cd6424b",
+    "url": "/DevFlix/static/js/main.f7b5b8bd.chunk.js"
+  },
+  {
+    "revision": "8c340a7da277faed5981",
+    "url": "/DevFlix/static/js/runtime-main.84224acd.js"
+  },
+  {
+    "revision": "388e61013491d751493f73673fc53021",
+    "url": "/DevFlix/static/media/Logo2.388e6101.png"
+  }
+]);
